1c587c7f35842aa124bfadb954a9e60881d4e90b
someidiot1234 <168358654+someidiot1234@users.noreply.github.com>
2025-07-02 23:12:53 -0700

    Networth Grapher
    Graphs networth against time.
    Requires your shiplist to be in a file titled 'shiplist.json' in the same folder as the program.

---
+++ downloader.py
@@ -0,0 +1,57 @@
+import urllib.request, sys, os, math, urllib.error
+from datetime import date, timedelta
+
+def getfilename(url):
+    "%Y_%m_%d_ships.json.gz"
+    # gets the index of "econ" in the url
+    info = url.index("econ")
+    # removes the econ part and the link before it
+    unformatted = (url[(info)+5:])
+    # formats name to use underscore instead of slash
+    unformatted = unformatted.replace("_", "-")
+    formatted = unformatted.replace("/", "_")
+    return formatted
+
+def getsize(path):
+   size_bytes = os.path.getsize(path)
+   if size_bytes == 0:
+       return "0 B"
+   size_name = ("","k","m","g","t","p","e","z","y","r","q")
+   i = int(math.floor(math.log(size_bytes, 1000)))
+   p = math.pow(1000, i)
+   s = round(size_bytes / p, 1)
+   output = str(s) + " " + size_name[i] + "B"
+   return output
+
+def main():
+    failed_links = ""
+    # EDIT THE DATES BELOW BEFORE RUNNING
+    #sys.exit(1)  # Remove < after you have confirmed lines 170 & 171
+    #start_date = date(2023, 4, 30)
+    start_date = date(2022, 11, 23)
+    end_date = date(2025, 7, 1)
+    delta = timedelta(days=1)
+    counter = 1
+    while start_date <= end_date:
+        url = start_date.strftime("https://pub.drednot.io/prod/econ/%Y_%m_%d/ships.json.gz")
+        url = url.replace('_0', '_')
+        output_filename = getfilename(url)
+        try:
+            req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+            with urllib.request.urlopen(req) as response:
+                data = response.read()
+            with open(output_filename, "wb") as f:
+                f.write(data)
+            print("Get:" + str(counter) + " " + url + " " + output_filename + " [" + getsize(output_filename) + "]")
+        except (urllib.error.HTTPError, urllib.error.URLError):
+            failed_links += url + "\n"
+            print("Miss:" + str(counter) + " " + url + " " + output_filename)
+        start_date += delta
+        counter += 1
+    with open("failed_links.txt", "w", encoding="utf-8") as file:
+        file.write(failed_links)
+    print("Completed sucessfully!")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
---
+++ networth.py
@@ -0,0 +1,112 @@
+# Imports
+from matplotlib import pyplot as plt
+from datetime import date, timedelta
+import sys, json, gzip
+import numpy as np
+
+# Classes
+class Vividict(dict):
+    # Fancy dictionary
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
+
+# Functions
+def networth(items):
+    # Calculates networth of a id:count list using a pricing list
+    networth = 0
+    with open("item_conversions.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    # item_conversions.json is formatted as id:npsf
+    # npsf = number per stack of flux
+    # Ex. 512 iron per stack of flux, so "1":"512"
+    for item in items:
+        # Reading item prices
+        if data[item] == "NaN":
+            print("Test-Only Item. Skipped. id is " + str(item))
+        elif data[item] == "inf":
+            # starter & debug items have no value
+            networth += 0
+        elif data[item] == "no value":
+            # eternals, I don't have pricing info
+            print("Rare Item with no market history. id is " + str(item))
+        else:
+            networth += int(items[item])/(float(data[item])/16)
+    return networth
+
+def formatshiplist(shiplist):
+    # Converts a shiplist json into a list of the hex ids
+    # filters for ships that haved 'owned' set to "True"
+    output = []
+    for ship in shiplist['ships']:
+        obj = shiplist['ships'][ship]
+        if obj['owned']:
+            output.append(obj['hex_code'])
+    return output
+
+def netstoplottable(days, shiplist):
+    # Converts the days object into a plottable object
+    # days is the networth of each ship you loaded every day
+    # output is the days networth every day you loaded a ship
+    latest_networth = Vividict()
+    output = Vividict()
+    for hex in shiplist:
+        latest_networth[hex] = 0
+    for day in days:
+        count = 0
+        for ship in days[day]:
+            latest_networth[ship] = days[day][ship]
+        for networth in latest_networth:
+            count += latest_networth[networth]
+        output[day] = count
+    return output
+
+def main():
+    # EDIT THE DATES BELOW BEFORE RUNNING
+    #sys.exit(1)  # Remove < after you have confirmed lines 170 & 171
+    start_date = date(2022, 11, 23)
+    end_date = date(2025, 7, 1)
+    delta = timedelta(days=1)
+    # Reading your shiplist
+    with open("shiplist.json", "r", encoding="utf-8") as f:
+        shiplist = formatshiplist(json.load(f))
+    # Iterating through dates
+    days = Vividict()
+    while start_date <= end_date:
+        # Calculating the input filename
+        filename = start_date.strftime("%Y-%m-%d_ships.json.gz").replace('-0', '-')
+        day = Vividict()
+        # Reading and Decompressing the file
+        with gzip.open(filename, "rb") as f:
+            data = json.load(f)
+        for item in data:
+            if item['hex_code'] in shiplist:
+                day[item['hex_code']] = networth(item['items'])
+        days[start_date] = day
+        start_date += delta
+    # converting the days object into a plottable object
+    networths = netstoplottable(days, shiplist)
+    # dividing the networths object into x and y values
+    times = []
+    nets = []
+    for worth in networths:
+        times.append(worth)
+        nets.append(networths[worth])
+    # Matplotlib plotting
+    plt.plot(times, nets)
+    # Axis labels
+    plt.xlabel("Date")
+    plt.ylabel("Networth (Flux)")
+    # Axis scaling (otherwise every day will have a tick mark)
+    ax = plt.gca()
+    xmin, xmax = ax.get_xlim()
+    custom_ticks = np.linspace(xmin, xmax, 5)#, dtype=str)
+    ax.set_xticks(custom_ticks)
+    # vv uncomment if you want Networth (they y axis) to be shown logarithmically
+    #ax.set_yscale('log')
+    plt.show()
+    print("Completed sucessfully!")
+    sys.exit(0)
+        
+if __name__ == "__main__":
+    main()
